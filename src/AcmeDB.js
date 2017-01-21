77bbf7b897e6b2aa189857e7a18d21433bf9c13d
Eric Katz <ericpkatz@gmail.com>
2017-01-21 10:18:17 -0500

    Profs comments...
    Good work!

--- src/AcmeDB.js
+++ src/AcmeDB.js
@@ -2,14 +2,16 @@ class AcmeDB{
     constructor({users}){
         this.users = users;
         this.users[0].id = 1;
+	//use addUser... loop over all users
     }
-    addUser(obj){
-       var maxId;
+    addUser(obj){//naming call this user
+       var maxId;//use reduce function to find the max
         maxId = this.users[this.users.length-1].id;
         this.users.push(obj);
         this.users[this.users.length-1].id = maxId +1;
     }
     showUsers(){
+	//shorten this up by using the map function.
         var userList = [];
 		this.users.forEach(function(user){
 			userList.push(user.name);
@@ -19,6 +21,7 @@ class AcmeDB{
     findById(id){
         //console.log(this.users);
         var target;
+	//instead use the map function and then join
         this.users.forEach(function(user){
             if(user.id ===id){
                 target =user;
@@ -28,6 +31,7 @@ class AcmeDB{
             return target;
     }
     removeUserById(id){
+	//use the filter function, it will be much cleaner
         var remove = 0;
 		this.users.forEach(function(user, index){
 			if (id === user.id){
@@ -38,7 +42,8 @@ class AcmeDB{
 
 
     }
-    editUser(obj){
+    editUser(obj){//editing call this editedUser
+	    //probably easiest to use map function- just replace the user within the map function callback
             var swap = 0;
 		this.users.forEach(function(user, index){
 			if (obj.id === user.id){
@@ -51,5 +56,5 @@ class AcmeDB{
     
 }
 
-console.log("hello")
+console.log("hello")//take these out... :)
 export default AcmeDB;
